2189606cb6192603d45cda0d2c4b67f145f27b07
Matthew Otto <matthewotto@utexas.edu>
2025-10-18 20:54:48 -0500

    add cmsis style headers

---
+++ src/inc/mcu.h
@@ -0,0 +1,10 @@
+#ifndef MCU_H
+#define MCU_H
+
+#include "cmsis_gcc.h"
+#include "core_armv8mml.h"
+#include "stm32h563xx.h"
+#include "stm32h563xx_gpio.h"
+#include "stm32h563xx_eth.h"
+
+#endif // MCU_H
